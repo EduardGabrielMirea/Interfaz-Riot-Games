ac975da84dd4db8f5da2074bb47b05ae6cea7029
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-11-11 13:03:07 +0100

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -25,11 +25,48 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    const acctions = {
+        'login-button': function () {
+            
+        },
+        'register-button': function () {
 
+        }
+    } 
+
+    
+    // Comprobar el From y redireccionar al home.html. Hay que hacer cuando le des enter que funcione también (Hecho por mi)
+    const registerButton = document.getElementById("register-button");
+    const loginHtml = '/templates/login/index.html';
+    const xhr = new XMLHttpRequest();
+    registerButton.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        //Coger los datos de los campos del formulario
+        var userName = document.getElementById("username").value.trim();
+        var password = document.getElementById("password").value.trim();
+        
+        // Llamar a la funcion de verificación
+        if (!verificarCampos(userName, password)) {
+            return; // Si la validación falla, detener el envío
+        }
+        // Falta verificar bien los datos.
+        xhr.open('GET', loginHtml, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // Si la autenticación es exitosa, redirigir a home.html
+                window.location.href = "/templates/login/index.html";
+            } else {
+                // Manejar el error de autenticación (opcional)
+                alert("Error de autenticación. Por favor verifica tus datos.");
+            }
+        }
+        xhr.send(null);
+    });
+    
     // Comprobar el From y redireccionar al home.html. Hay que hacer cuando le des enter que funcione también (Hecho por mi)
     const loginButton = document.getElementById("login-button");
     const homeHtml = '/templates/home/index.html';
-    const xhr = new XMLHttpRequest();
     loginButton.addEventListener("click", function (event) {
         event.preventDefault();
 
@@ -54,35 +91,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         xhr.send(null);
     });
-
-    // Comprobar el From y redireccionar al home.html. Hay que hacer cuando le des enter que funcione también (Hecho por mi)
-    const registerButton = document.getElementById("register-button");
-    const loginHtml = '/templates/login/index.html';
-    registerButton.addEventListener("click", function (event) {
-        event.preventDefault();
-
-        //Coger los datos de los campos del formulario
-        var userName = document.getElementById("username").value.trim();
-        var password = document.getElementById("password").value.trim();
-
-        // Llamar a la funcion de verificación
-        if (!verificarCampos(userName, password)) {
-            return; // Si la validación falla, detener el envío
-        }
-        // Falta verificar bien los datos.
-        xhr.open('GET', loginHtml, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                // Si la autenticación es exitosa, redirigir a home.html
-                window.location.href = "/templates/login/index.html";
-            } else {
-                // Manejar el error de autenticación (opcional)
-                alert("Error de autenticación. Por favor verifica tus datos.");
-            }
-        }
-        xhr.send(null);
-    });
-
 
     // Script para mostrar u ocultar la contraseña en el login.
     var passwordInput = document.getElementById("password");
